d55c9cc86ea03cfba55195182d749c31b57e79e9
Vaibhav <vscoderr@gmail.com>
2024-08-10 14:30:52 +0530

    commit

--- app.js
+++ app.js
@@ -1,61 +1,51 @@
-
 // ? SELECT DOM ELEMENTS TO INTEREACT
-const userPassword = document.getElementById('pass-input');
-const hideAndShow = document.querySelector('.container img');
-const strengthCheck = document.getElementById('strength');
+const userPassword = document.getElementById("pass-input");
+const hideAndShow = document.querySelector(".container img");
+const strengthCheck = document.getElementById("strength");
 
+userPassword.addEventListener("input", () => {
+  let userPassowordValue = userPassword.value;
 
-userPassword.addEventListener('input',() => {
+  if (userPassowordValue.length == 0) {
+    setTimeout(() => {
+      userPassword.style.borderColor = "red";
+      strengthCheck.innerHTML = "Enter password";
+      strengthCheck.style.color = "red";
+      userPassword.classList.toggle("animate__headShake");
+    }, 200);
+  } else if (userPassowordValue.length == 1) {
+    setTimeout(() => {
+      userPassword.style.borderColor = "gold";
+      strengthCheck.innerHTML = "Password is too Weak";
+      strengthCheck.style.color = "gold";
+    }, 200);
+  } else if (userPassowordValue.length == 5) {
+    setTimeout(() => {
+      userPassword.style.borderColor = "tomato";
+      strengthCheck.innerHTML = "Password is too Medium";
+      strengthCheck.style.color = "tomato";
+    }, 200);
+  } else if (userPassowordValue.length == 8) {
+    setTimeout(() => {
+      userPassword.style.borderColor = "#5bea5b";
+      strengthCheck.innerHTML = "Password is Strong";
+      strengthCheck.style.color = "#5bea5b";
+    }, 200);
+  } else if (userPassowordValue.length > 8) {
+    setTimeout(() => {
+      userPassword.style.borderColor = "red";
+      strengthCheck.innerHTML = "Password must be 8 Characters only";
+      strengthCheck.style.color = "red";
+    }, 200);
+  }
+});
 
-   let userPassowordValue = userPassword.value;
-
-   if(userPassowordValue.length == 0){
-      setTimeout(() => {
-         userPassword.style.borderColor = 'red';
-      strengthCheck.innerHTML = 'Enter password';
-      strengthCheck.style.color = 'red'
-         userPassword.classList.toggle('animate__headShake')
-      }, 200);
-   }
-   else if(userPassowordValue.length == 1){
-      setTimeout(() => {
-         userPassword.style.borderColor = 'gold';
-      strengthCheck.innerHTML = 'Password is too Weak';
-      strengthCheck.style.color = 'gold'
-      }, 200);
-   }
-   else if(userPassowordValue.length == 5){
-      setTimeout(() => {
-         userPassword.style.borderColor = 'tomato';
-      strengthCheck.innerHTML = 'Password is too Medium';
-      strengthCheck.style.color = 'tomato'
-      }, 200);
-   }
-   else if(userPassowordValue.length == 8){
-      setTimeout(() => {
-         userPassword.style.borderColor = '#5bea5b';
-      strengthCheck.innerHTML = 'Password is Strong';
-      strengthCheck.style.color = '#5bea5b'
-      }, 200);
-   }
-   else if(userPassowordValue.length > 8){
-      setTimeout(() => {
-         userPassword.style.borderColor = 'red';
-      strengthCheck.innerHTML = 'Password must be 8 Characters only';
-      strengthCheck.style.color = 'red'
-      }, 200);
-   }
-
-})
-
-hideAndShow.addEventListener('click',() => {
-   if(userPassword.type === 'password'){
-      userPassword.type = 'text'
-      hideAndShow.src = "images/eye.svg"
-   }
-   else{
-      userPassword.type = 'password'
-      hideAndShow.src = "images/eye-off.svg"
-   }
-})
-
+hideAndShow.addEventListener("click", () => {
+  if (userPassword.type === "password") {
+    userPassword.type = "text";
+    hideAndShow.src = "images/eye.svg";
+  } else {
+    userPassword.type = "password";
+    hideAndShow.src = "images/eye-off.svg";
+  }
+});
